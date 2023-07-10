d7de9a9b7fbc908b639422b3d695e7fd899b4ec6
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-07-10 10:41:06 -0500

    Se adaptó TestBed a las pruebas

--- src/services/master/master.service.spec.ts
+++ src/services/master/master.service.spec.ts
@@ -1,25 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MasterService } from './master.service';
+import { ValueService } from '../value/value.service';
 
 describe('MasterService', () => {
-  let service: MasterService;
+  let masterService: MasterService;
+  let valueServiceSpy: jasmine.SpyObj<ValueService>;  
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MasterService);
+    const spy = jasmine.createSpyObj('ValueService', ['getValue']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        MasterService,
+        { provide: ValueService, useValue: spy },
+      ]
+    });
+
+    masterService = TestBed.inject(MasterService);
+    valueServiceSpy = TestBed.inject(ValueService) as jasmine.SpyObj<ValueService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(masterService).toBeTruthy();
   });
 
   it('deberia llamar getValue de ValueService', () => {
-    const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
-    const masterService = new MasterService(valueServiceSpy);
-
     valueServiceSpy.getValue.and.returnValue('fake value');
-
     expect(masterService.getValue()).toBe('fake value');
     expect(valueServiceSpy.getValue).toHaveBeenCalled();
     expect(valueServiceSpy.getValue).toHaveBeenCalledTimes(1);
